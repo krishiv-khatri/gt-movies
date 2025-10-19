177f81b6fb1281c03bda33088df3146018fd0f20
Krishiv Khatri <krishivkhatri2409@gmail.com>
2025-10-19 18:41:19 -0400

    fixed profiles bug

--- store/models.py
+++ store/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Movie(models.Model):
@@ -181,10 +179,3 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity}x {self.movie.title} in Order #{self.order.id}"
-
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    else:
-        instance.profile.save()
